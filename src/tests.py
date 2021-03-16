b89e2c861212f1ea37be6d381d2c6262f813fc5e
kaigikinyua <kaigikinyua@gmail.com>
2021-03-16 20:52:40 +0300

    cleaning up tests

--- src/tests.py
+++ src/tests.py
@@ -6,13 +6,14 @@ from entities import *
 from gen import *
 
 class EntitiesTest(unittest.TestCase):
+#user entity    
     def test_user(self):
         u=User()
         user=u.gen_fulluser()
         Messages.success("\nPrinting generated user \n {l}\n".format(l=str(user)))
         self.assertNotEqual(user,None)
         self.assertEqual(type(user),type({}))
-
+#location entity
     def test_location(self):
         l=Location()
         addr=l.gen_address()
@@ -21,14 +22,14 @@ class EntitiesTest(unittest.TestCase):
         Messages.success("\nPrinting generated coordinates \n {l}\n".format(l=str(cord)))
         self.assertEqual(type(addr),type({}))
         self.assertEqual(type(cord),type({}))
-
+#product
     def test_product(self):
         p=Product()
         full_prod=p.gen_fullProduct()
         prod_reviews=p.gen_productReviews(3)
         Messages.success("\nPrinting generated full product {l} \n".format(l=str(full_prod)))
         self.assertEqual(len(prod_reviews),3)
-    
+#texb blob    
     def test_textBlob(self):
         tB=TextBlob()
         lP=tB.gen_paragraph(size="l")
@@ -39,12 +40,12 @@ class EntitiesTest(unittest.TestCase):
         paragraphs=[lP,sP,mP,noParams,upperCaseParam]
         for par in paragraphs:
             self.assertEqual(type(par),type({}))
-            self.assertNotEqual(par,None)
+            self.assertGreater(len(par),0)
         lists=tB.gen_List(l=2)
-        Messages.success("Printing generated list {l}".format(l=str(lists)))
-        self.assertNotEqual(len(lists),0)
+        #Messages.success("Printing generated list {l}".format(l=str(lists)))
+        #self.assertNotEqual(len(lists),0)
+        self.assertGreater(len(lists),0)
 
-#code base tests
 class GenTest(unittest.TestCase):
 #User Generation
     def test_randomUserGeneration(self):
@@ -63,53 +64,6 @@ class GenTest(unittest.TestCase):
         users=UserGen.gen_user_reviews(10)
         #Messages.success("\nPrinting generated reviews\n {l} \n".format(l=str(users)))
         self.assertEqual(len(users),10)
-
-"""#Users Reviews
-    def test_randomReviews(self):
-        userReviews=UserReview.random_reviews(10)
-        self.assertEqual(len(userReviews),10)
-#Blogs
-    def test_randomBlog(self):
-        blog=Blogs.randomBlog()
-        self.assertEqual(len(blog),9)
-    def test_blogSnippets(self):
-        blogSnippets=Blogs.blogSnippets(1)
-        self.assertEqual(len(blogSnippets),1)
-#lists
-    def test_nestedList(self):
-        l=List.nested_list(3)
-        self.assertEqual(len(l),3)
-    def test_simpleList(self):
-        l=List.simple_list(3)
-        self.assertEqual(len(l),3)
-#products
-    def test_products(self):
-        specificPrdct=Products.getProductsInCategory("fashion")
-        randomPrdct=Products.genRandomProductList()
-        allPrdct=Products.getAllProductList()
-        self.assertIsNotNone(specificPrdct)
-        self.assertIsNotNone(randomPrdct)
-        self.assertIsNotNone(allPrdct)
-
-"""
-class MyDBTest:
-    pass
-
-class ModelsTest:
-    pass
-
-class RequestTest:
-    pass
-
-class UtilityTest:
-    pass
-
-#resources
-class SchemaTest:
-    pass
-
-class GenericDataTest:
-    pass
 
 if __name__=="__main__":
     unittest.main()
