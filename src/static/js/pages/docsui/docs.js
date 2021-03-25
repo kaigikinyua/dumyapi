3299aa15b980959e18f50300999e9b9632696c3f
kaigikinyua <kaigikinyua@gmail.com>
2021-03-25 16:37:12 +0300

    added vuejs to docs and added search functionality to docs.html

--- src/static/js/pages/docsui/docs.js
+++ src/static/js/pages/docsui/docs.js
@@ -2,4 +2,44 @@
     //components -> appbar
     //page -> paragraph & titles
 
-    
+const baseURL="http://localhost:5000"
+
+let contentvue=new Vue({
+    el:'#docsview',
+    delimiters:['[[',']]'],
+    data(){
+        return {
+            title:'Test Title',
+            content:[]
+        }
+    }
+})
+
+function renderDoc(doclink){
+    fetchDocs(doclink,(data)=>{
+        if(data.title && data.items){
+            contentvue.title=data.title
+            contentvue.content=data.items
+        }
+    })
+}
+function fetchDocs(url,callback){
+    fetch(`${baseURL}${url}`)
+    .then(res=>res.json())
+    .then(data=>{callback(data)})
+}
+
+
+//search functionality
+const searchInput=document.getElementById("search")
+searchInput.addEventListener('keydown',(e)=>{
+    var input=searchInput.value
+    var tiles=document.querySelectorAll('div.tile')
+    tiles.forEach(t=>{
+        if(t.children[0].innerHTML.indexOf(input)==-1){
+            t.style.display="none"
+        }else{
+            t.style.display="block"
+        }
+    })
+})
