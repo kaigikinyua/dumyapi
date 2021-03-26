ca6d354ea41d888195be6cc00e9a0a724e4fa833
kaigikinyua <kaigikinyua@gmail.com>
2021-03-26 22:48:17 +0300

    adding the product and users api endpoint to src.apps.py

--- src/app.py
+++ src/app.py
@@ -6,13 +6,15 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-    
+
+#generic data webpage
 @app.route('/genericdata')
 def generic_data_page():
     gDList=GenericData.listGenericDataFiles()
     request_url="/static/genericdata"
     return render_template('genericdata.html',gDList=gDList,rUrl=request_url)
-#api endpoints
+
+#generic data endpoint
 @app.route('/getdata/<genericdata>/<number>')
 def getdata(genericdata,number=100):
     data=GenericData.genericDataActions(genericdata,int(number))
@@ -26,6 +28,32 @@ def getdata(genericdata,number=100):
         response=jsonify({"state":"error","genericdata":{},"message":"There seems to be an error getting generic data {g}".format(g=genericdata)})
     return response
 
+
+#dummy users
+@app.route('/users/<profileType>/<number>')
+def getUsersProfiles(profileType='fullprofile',number=10):
+    usersData={"name":"Test"}
+    if(profileType=='fullprofile'):
+        pass
+    elif(profileType=='miniprofile'):
+        pass
+    else:
+        pass
+    return jsonify(usersData)
+
+#dummy products
+@app.route('/products/<scope>/<number>')
+def getProducts(scope='full',number=10):
+    productData={"name":"Test"}
+    if(scope=='fullproduct'):
+        pass
+    else:
+        pass
+    return jsonify(productData)
+
+
+
+#documentation
 @app.route('/documentation')
 def documentation():
     doclist=JsonFile.loadData('./static/webdocs/index.json')
