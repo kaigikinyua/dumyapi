135b1093394ec81d7828b06eddca8ebc57928b2f
kaigikinyua <kaigikinyua@gmail.com>
2021-04-05 07:07:59 +0300

    array of user definded objects done for the dummy data api language extrapolator

--- src/customJson.py
+++ src/customJson.py
@@ -7,24 +7,42 @@ class CustomJson:
 
     def populateJson(self):
         arrays=Parser.filterArrays(self.datastring)
-        objects=Parser.filterObjects(self.datastring)
-        print(objects)
         datastringreplacements=[]
         for arr in arrays:
             var=arr.split('...')
-            rep=Parser.expandVariables(var[1])
-            datastringreplacements+=[{"variable":arr,"rep":rep}]
+            number=int(var[1][0:len(var[1])-1])
+            arrObjects=Parser.filterObjects(arr)
+            if(len(arrObjects)>0):
+                rep=""
+                while (number>0):
+                    s="{"
+                    for obj in arrObjects:
+                        o=obj.split(":")
+                        value=Parser.expandVariable(o[1])
+                        if(obj==arrObjects[len(arrObjects)-1]):
+                            s+="{k}:'{v}'".format(k=o[0],v=value)
+                        else:
+                            s+="{k}:'{v}',".format(k=o[0],v=value)
+                    if(number==1):
+                        s+="}"
+                    else:
+                        s+="},"
+                    rep+=s
+                    number=number-1
+                datastringreplacements+=[{"variable":arr[1:len(arr)-1],"rep":rep}]
+        objects=Parser.filterObjects(self.datastring)
         for obj in objects:
             var=obj.split(":")
+            rep=""
             rep=Parser.expandVariable(var[1])
-            #print(rep)
             datastringreplacements+=[{"variable":obj,"rep":"{k}:{v}".format(k=var[0],v=rep)}]
         return Parser.compileString(self.datastring,datastringreplacements)
 
 class Parser:
     @staticmethod
     def filterArrays(datastring):
-        return re.findall('\.\.\.[\w\d]*',datastring)
+        #enable ddapi to 'extrapolate' array of objects ['name':$username]|<number of objects>
+        return re.findall("\[[\w\d,':]*\.\.\.[\d]+\]",datastring)
     @staticmethod
     def filterObjects(datastring):
         return re.findall("['\w]+:[\w]+",datastring)
@@ -89,7 +107,7 @@ class Variables:
 
 if __name__=="__main__":
     #print(sys.argv)
-    #print(Variables.categorize("lastname"))
-    datastring="{'firstname':firstname,'lastname':lastname,'email':email}"
+    #print(Variables.categorize("lastname")) 
+    datastring="{'firstname':firstname,'lastname':lastname,'email':email,'friends':['name':username,'email':email...2],'messages':[username...10]"
     c=CustomJson(datastring)
     print(c.populateJson())
