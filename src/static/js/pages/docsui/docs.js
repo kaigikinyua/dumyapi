6ee28bd6d81967fe67f67eb408f1484de9c12ddb
kaigikinyua <kaigikinyua@gmail.com>
2021-04-09 21:34:49 +0300

    styling the documentation navigation bar

--- src/static/js/pages/docsui/docs.js
+++ src/static/js/pages/docsui/docs.js
@@ -4,6 +4,21 @@
 
 const baseURL="http://localhost:5000"
 
+//ui logic
+function currentSelected(datasetIdentifier){
+    var liTags=document.querySelectorAll('li.genericDataButton')
+    liTags.forEach(t=>{
+        console.log(t)
+        if(t.dataset["litag"]==datasetIdentifier){
+            t.classList.add('active')
+        }else{
+            t.classList.remove('active')
+        }
+    });
+}
+
+
+
 let contentvue=new Vue({
     el:'#docsview',
     delimiters:['[[',']]'],
@@ -16,6 +31,7 @@ let contentvue=new Vue({
 })
 
 function renderDoc(doclink){
+    currentSelected(doclink)
     fetchDocs(doclink,(data)=>{
         if(data.title && data.items){
             contentvue.title=data.title
